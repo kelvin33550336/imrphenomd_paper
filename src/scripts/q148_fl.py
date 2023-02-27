1dc87e70b954a69c41f86b19a0c65013a83e5163
Kelvin Lam <kelvin33550336@gmail.com>
2023-02-26 16:24:42 -0800

    fix all

--- src/scripts/q148_fl.py
+++ src/scripts/q148_fl.py
@@ -6,8 +6,8 @@ import paths
 data = np.loadtxt(paths.data / "q148_fl.dat")
 
 bins = np.arange(-5, -1, 0.25)
-plt.hist(np.log10(data[4]).values, bins=bins, alpha=0.6, label='original')
-plt.hist(np.log10(data[5].values), bins=bins, alpha=0.6, label='optimized')
+plt.hist(np.log10(data[:,4]), bins=bins, alpha=0.6, label='original')
+plt.hist(np.log10(data[:,5]), bins=bins, alpha=0.6, label='optimized')
 plt.legend()
 plt.ylabel('Count')
 plt.xlabel('$\log(\mathrm{mismatch})$')
