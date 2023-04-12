2acb40680b5dbdcb1ab3f219799bc236ed12ba59
Kelvin Lam <kelvin33550336@gmail.com>
2023-04-11 22:45:49 -0700

    update

---
+++ src/scripts/loss.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import paths
+
+params = {
+    "font.size": 18,
+    "legend.fontsize": 12,
+    "legend.frameon": True,
+    "axes.labelsize": 16,
+    "axes.titlesize": 16,
+    "xtick.labelsize": 14,
+    "ytick.labelsize": 14,
+    "axes.unicode_minus": False,
+    "figure.figsize": (7, 5),
+    "xtick.top": True,
+    "ytick.right": True,
+    "xtick.bottom": True,
+    "ytick.left": True,
+    "xtick.major.pad": 8,
+    "xtick.major.size": 8,
+    "xtick.minor.size": 4,
+    "ytick.major.size": 8,
+    "ytick.minor.size": 4,
+    "xtick.direction": "in",
+    "ytick.direction": "in",
+    "axes.linewidth": 1.5,
+    "text.usetex": False,
+    "font.family": "serif",
+    "font.serif": "cmr10",
+    "mathtext.fontset": "cm",
+    "axes.formatter.use_mathtext": True,  # needed when using cm=cmr10 for normal text
+}
+
+mpl.rcParams.update(params)
+
+data = np.loadtxt(paths.data / "q148_loss.dat")
+
+plt.plot(data[:,0], data[:,1])
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.savefig(paths.figures / "loss.pdf", bbox_inches="tight")
