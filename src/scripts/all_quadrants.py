2436322cc9284794099e507c7c40fe8207ed4803
Kelvin Lam <kelvin33550336@gmail.com>
2023-06-21 18:10:18 -0700

    added citation and fixed some comment

--- src/scripts/all_quadrants.py
+++ src/scripts/all_quadrants.py
@@ -36,18 +36,33 @@ mpl.rcParams.update(params)
 
 c1 = "#102F68"
 c2 = "#B02423"
+c3 = "#FFB300"
 
 data2 = np.loadtxt(paths.data / "1neg2pos_mismatch.dat")
 data4 = np.loadtxt(paths.data / "1pos2neg_mismatch.dat")
+
+data = np.loadtxt(paths.data / "q148.dat")
+data2_q148 = np.array([data[i, 5] for i in range(len(data[:,0])) if data[i, 2] < 0 and data[i, 3] > 0])
+data4_q148 = np.array([data[i, 5] for i in range(len(data[:,0])) if data[i, 2] > 0 and data[i, 3] < 0])
 
 bins = np.linspace(-5, -1, 15)
 
 fig, ax = plt.subplots(2, figsize=(6, 10), sharex=True)
 plt.subplots_adjust(hspace=0.05)
 
-ax[0].hist(np.log10(data2[:, 4]), bins=bins, alpha=0.2, color=c1)
 ax[0].hist(
-    np.log10(data2[:, 4]), bins=bins, label="Original", color=c1, lw=2, histtype="step"
+    np.log10(data2[:, 4]), 
+    bins=bins, 
+    alpha=0.2, 
+    color=c1
+)
+ax[0].hist(
+    np.log10(data2[:, 4]), 
+    bins=bins, 
+    label="Original", 
+    color=c1, 
+    lw=2, 
+    histtype="step"
 )
 ax[0].axvline(
     x=np.log10(np.median(data2[:, 4])),
@@ -62,17 +77,38 @@ ax[0].hist(
     bins=bins,
     label="Optimized",
     histtype="step",
-    color=c2,
+    color=c3,
     linewidth=2,
 )
 ax[0].hist(
     np.log10(data2[:, 5]),
     bins=bins,
-    color=c2,
+    color=c3,
     alpha=0.2,
 )
 ax[0].axvline(
     x=np.log10(np.median(data2[:, 5])),
+    color=c3,
+    lw=2,
+    alpha=1.0,
+    linestyle=(0, (1, 1.3)),
+)
+ax[0].hist(
+    np.log10(data2_q148), 
+    bins=bins, 
+    alpha=0.2, 
+    color=c2
+)
+ax[0].hist(
+    np.log10(data2_q148), 
+    bins=bins, 
+    label="Optimized $(\mathcal{L}_{\mathrm{ave}})$", 
+    color=c2, 
+    lw=2, 
+    histtype="step"
+)
+ax[0].axvline(
+    x=np.log10(np.median(data2_q148)),
     color=c2,
     lw=2,
     alpha=1.0,
@@ -81,8 +117,19 @@ ax[0].axvline(
 ax[0].set_ylabel("Count")
 ax[0].legend(loc="upper left")
 
-ax[1].hist(np.log10(data4[:, 4]), bins=bins, alpha=0.2, color=c1)
-ax[1].hist(np.log10(data4[:, 4]), bins=bins, histtype="step", color=c1, lw=2)
+ax[1].hist(
+    np.log10(data4[:, 4]), 
+    bins=bins, 
+    alpha=0.2, 
+    color=c1
+)
+ax[1].hist(
+    np.log10(data4[:, 4]), 
+    bins=bins, 
+    histtype="step", 
+    color=c1, 
+    lw=2
+)
 ax[1].axvline(
     x=np.log10(np.median(data4[:, 4])),
     color=c1,
@@ -91,10 +138,41 @@ ax[1].axvline(
     alpha=1.0,
     linestyle=(0, (1, 1.3)),
 )
-ax[1].hist(np.log10(data4[:, 5]), bins=bins, histtype="step", color=c2, lw=2)
-ax[1].hist(np.log10(data4[:, 5]), bins=bins, color=c2, alpha=0.2)
+ax[1].hist(
+    np.log10(data4[:, 5]), 
+    bins=bins, 
+    histtype="step", 
+    color=c3, 
+    lw=2
+)
+ax[1].hist(
+    np.log10(data4[:, 5]), 
+    bins=bins, 
+    color=c3, 
+    alpha=0.2
+)
 ax[1].axvline(
     x=np.log10(np.median(data4[:, 5])),
+    color=c3,
+    lw=2,
+    alpha=1.0,
+    linestyle=(0, (1, 1.3)),
+)
+ax[1].hist(
+    np.log10(data4_q148), 
+    bins=bins, 
+    alpha=0.2, 
+    color=c2
+)
+ax[1].hist(
+    np.log10(data4_q148), 
+    bins=bins, 
+    color=c2, 
+    lw=2, 
+    histtype="step"
+)
+ax[1].axvline(
+    x=np.log10(np.median(data4_q148)),
     color=c2,
     lw=2,
     alpha=1.0,
